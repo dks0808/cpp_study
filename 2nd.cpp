9f92135aaa9ece5cd3b9e1e05d3494acb9108e90
light <rhkddbs0808@naver.com>
2024-01-24 18:46:35 +0900

    solving simple problems

--- 2nd.cpp
+++ 2nd.cpp
@@ -4,11 +4,10 @@ using namespace  std;
 
 int main()
 {
-    double result = 1;
-
-    for(int i=0;i<10;i++)
-        cout << fixed << setprecision(i) << result <<endl;  
-    return 0;
+    string name;
+    cout << name;
+    return 1;
+    
 }
 
 
