2eaa8950903bd078c3e273da683feb58c8590e21
light <rhkddbs0808@naver.com>
2024-01-24 18:22:59 +0900

    float expression

--- 2nd.cpp
+++ 2nd.cpp
@@ -1,34 +1,14 @@
 #include <iostream>
-using namespace std;
+#include <iomanip> //////////////////
+using namespace  std;
 
 int main()
 {
-    double num1,num2;
-    
-    cout << "첫번째 숫자 입력: " ;
-    cin>> num1;
+    double result = 1;
 
-    cout<<"두번째 숫자 입력: ";
-    cin >>num2;
-
-    if (num1 != num2)
-    {
-        if (num1 > num2)
-        {
-            cout << num1 << " > " << num2;
-        }
-        else 
-        {
-            cout << num2 << " > " << num1;
-        }
-    }
-    else 
-    {
-        cout<< num1 << " = " <<num2;
-
-    }
-
+    for(int i=0;i<10;i++)
+        cout << fixed << setprecision(i) << result <<endl;  
     return 0;
-    
-
 }
+
+
