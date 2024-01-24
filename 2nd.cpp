62cf07e42ddc3e437c75f8d4e40980c8436c8795
light <rhkddbs0808@naver.com>
2024-01-24 15:15:44 +0900

    3

--- 2nd.cpp
+++ 2nd.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
+
 int main()
 {
-    
-    double number ;
-    int intpart;
-    double fractpart;
+    unsigned int givenInt, firstDigit;
+    cout << "양의 정수 입력: ";
+    cin >>givenInt;
 
-    cout << "부동소수점 입력:";
-    cin >> number;
-
-    intpart = static_cast<int>(number);
-    fractpart =number -intpart;
-
-    cout << fixed << showpoint << setprecision;
-    cout <<"원래값: "<< number <<endl;
-    cout <<"정수부분:" << intpart <<endl;
-    cout << "소수점 아래부분:" << fractpart;
+    firstDigit = givenInt%10;
+    cout<<"입력한 정수: " << givenInt << endl;
+    cout <<"첫번째 자릿수 추출: " <<firstDigit<<endl;
     return 0;
-
 }
