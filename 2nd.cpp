819898cdebb7626ed78e8ace5663a1636f5719e5
light <rhkddbs0808@naver.com>
2024-01-24 16:16:27 +0900

    practice 4.5

--- 2nd.cpp
+++ 2nd.cpp
@@ -3,12 +3,32 @@ using namespace std;
 
 int main()
 {
-    unsigned int givenInt, firstDigit;
-    cout << "양의 정수 입력: ";
-    cin >>givenInt;
+    double num1,num2;
+    
+    cout << "첫번째 숫자 입력: " ;
+    cin>> num1;
 
-    firstDigit = givenInt%10;
-    cout<<"입력한 정수: " << givenInt << endl;
-    cout <<"첫번째 자릿수 추출: " <<firstDigit<<endl;
+    cout<<"두번째 숫자 입력: ";
+    cin >>num2;
+
+    if (num1 != num2)
+    {
+        if (num1 > num2)
+        {
+            cout << num1 << " > " << num2;
+        }
+        else 
+        {
+            cout << num2 << " > " << num1;
+        }
+    }
+    else 
+    {
+        cout<< num1 << " = " <<num2;
+
+    }
+
     return 0;
+    
+
 }
