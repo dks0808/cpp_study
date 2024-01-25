f89c7f38b33900f57be7e139c433948a5bc58e40
light <rhkddbs0808@naver.com>
2024-01-25 15:44:10 +0900

    functions declaration, definition, overloding

--- 3rd.cpp
+++ 3rd.cpp
@@ -5,15 +5,17 @@
 using namespace std;
 
 int fun(int a, int b);
-
 int fun(int a);
-
-
+int fun(char a);
 
 int main()
 {
-    cout <<"This is function 1 overloading test: " << fun(3,4) << endl;
-    cout <<"This is function 2 overloading test: " << fun(10) << endl;    
+    int a = 141;
+    int b = 128;
+    char c = 'a';
+    cout <<"This is function 1 overloading test: " << fun(a,b) << endl;
+    cout <<"This is function 2 overloading test: " << fun(b) << endl;   
+    cout <<"This is function 3 overloading test: " << fun(c) << endl;    
     
     return 0; 
     
@@ -33,3 +35,9 @@ int fun(int a)
 
 }
 
+int fun(char a)
+{
+    cout << a*3 <<endl;
+    return 2;
+
+}
