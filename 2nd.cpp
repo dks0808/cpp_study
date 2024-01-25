9d86001f254d4c9f43e1d2bbaad5e23962d6cd11
light <rhkddbs0808@naver.com>
2024-01-25 10:17:45 +0900

    3rd file

--- 2nd.cpp
+++ 2nd.cpp
@@ -4,14 +4,7 @@ using namespace  std;
 
 int main()
 {
-    int counter = 0;
-
-    while (counter <10) ;
-    {
-        cout << "Hello world!" <<endl;
-        counter ++;
-
-    }
+    for ()
     
 
 
