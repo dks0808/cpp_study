10042654ea7727533ffa58701501cad7ecf21132
light <rhkddbs0808@naver.com>
2024-01-26 14:16:05 +0900

    class and constructor, destructor

--- 3rd.cpp
+++ 3rd.cpp
@@ -1,43 +1,60 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-
+#include<ctime>
 using namespace std;
 
-int fun(int a, int b);
-int fun(int a);
-int fun(char a);
+class RandomInteger
+{
+    private:
+        int low;
+        int high;
+        int value;
+    public:
+        RandomInteger(int low, int high);
+        ~RandomInteger();
+
+        RandomInteger(const RandomInteger& random) = delete;
+        void print() const;
+
+};
+// 생성자
+RandomInteger::RandomInteger(int lw, int hh)
+:low(lw), high(hh)
+{
+    srand(time(0));
+    int temp = rand();
+    value = temp % (high-low+1) + low;
+
+}
+
+//소멸자
+RandomInteger::~RandomInteger()
+{
+
+}
+
+void RandomInteger::print() const
+{
+    cout << value << endl;
+}
+
 
 int main()
-{
-    int a = 141;
-    int b = 128;
-    char c = 'a';
-    cout <<"This is function 1 overloading test: " << fun(a,b) << endl;
-    cout <<"This is function 2 overloading test: " << fun(b) << endl;   
-    cout <<"This is function 3 overloading test: " << fun(c) << endl;    
-    
+{   
+
+    RandomInteger r1(100, 200);
+    cout << "between 100~200 random number: ";
+    r1.print();
+
+    RandomInteger r2(400, 600);
+    cout << "between 400~600 random number: ";
+    r2.print();
+
+    RandomInteger r3(1500, 2000);
+    cout << "between 1500~2000 random number: ";
+    r3.print();
+
     return 0; 
     
-}
-//
-int fun(int a, int b)
-{ 
-    int c = a+b;
-    cout << c << endl ;
-    return 1;
-}
-
-int fun(int a)
-{
-    cout << ++a << endl;
-    return 2;
-
-}
-
-int fun(char a)
-{
-    cout << a*3 <<endl;
-    return 2;
-
 }
