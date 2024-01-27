fcc7200904d1f8ea26fb6a87508a36bf038fbf06
light <agy91521947@gmail.com>
2024-01-27 15:50:12 +0900

    star print

--- 4th .cpp
+++ 4th .cpp
@@ -1,36 +1,18 @@
 #include <iostream>
-
-int minites(int s);
-int hours(int s);
-
 using namespace std;
+
 int main(){
-    int h;
-    int m;
-    int s3;
-    cout << "Enter the hour: " << endl;
-    cin >> h;
-    cout << "Enter the minites: " << endl;
-    cin >> m;
-    cout << "Enter the seconds: " << endl;
-    cin >> s3;
+    char a = '*';
     
-    int s1 = hours(h);
-    int s2 = minites(h);
-    cout << "The time: " << s1 << " s"<<endl; 
-    cout << "The time: " << s2 <<" s"<<endl; 
-    cout << "The timeu: " << s3<<" s"<<endl; 
+    
+    cout << a << endl;
+    cout << a << a<< endl;
+    cout << a <<a<< a<< endl;
+    cout << a <<a<< endl;
+    cout << a << endl;
 
     return 0;
-}
 
-int minites(int s)
-{
-    int a = s * 60;
-    return a;
-}
-int hours(int s)
-{
-    int a = minites(s) * 60;
-    return a;
+
+    
 }
