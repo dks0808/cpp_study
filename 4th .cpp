0e21717978fa16e2b8a0b7b2ee4aa7f3962ae809
light <agy91521947@gmail.com>
2024-01-27 17:12:08 +0900

    3-31 get average

--- 4th .cpp
+++ 4th .cpp
@@ -3,19 +3,27 @@ using namespace std;
 
 int main()
 {
-   unsigned durations, hours, minutes, seconds;
-   cout << "Enter duration time: " ;
-   cin >> durations ;
+
+   int a, b, c;
+   int sum;
+   float mean;
+   cout << "Enter the each score 3: "<<endl;
+   cin >> a >> b >> c;
+   sum = a+b+c;
+   mean = sum/3;
    
-   hours = durations/3600;
-   minutes = (durations-(hours*3600))/60;
-   seconds = durations -(hours*3600)-(minutes*60);
+   cout << "first score is " << a<< endl;
+   cout << "second score is "<< b<< endl;
+   cout << "third score is "<<c <<endl;
    
 
-   cout << "duration time : " << durations<<" s" <<endl ;
-   cout <<  hours<<"h ";
-   cout <<  minutes<<"m ";
-   cout << seconds<<"s "<<endl;
+   cout << "sum: " << sum << endl;
+   cout << fixed << showpoint;
+   cout << "mean: " << mean << endl;
 
+    
+   
+   
+    
     return 0;
 }
