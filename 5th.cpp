9b6e1c3fd56fa6478baee12cc2101f0175c40472
light <agy91521947@gmail.com>
2024-01-28 16:49:07 +0900

    factorialby using for

--- 5th.cpp
+++ 5th.cpp
@@ -5,29 +5,18 @@ using namespace std;
 
 int main()
 {
-    int n; 
-    long double number;
-
-    do
-    {
-        cout << "Enter the repeat number: "<< endl;
-        cin >> n;
-    } while (n <= 0);
-
-    long double sum = 0;
-    long double tsum = 1;   
     
-    for(int count = 0; count < n; count++)
-    {
-        cout << "Enter the numbers which you want : "<< endl;
-        cin >> number;
-        sum = sum + number;
-        tsum = tsum * number;
-    } 
-    
-    cout << fixed << setprecision(5) << showpoint;
-    cout << "Sum of numbers that you entered : " << sum << endl;
-    cout << "Product of numbers that you entered : " << tsum << endl;
+    unsigned long long Factorial=1; 
+    int n ;
+    cout << "Enter the number you want: ";
+    cin >> n;
 
+    for(int i = 1; i<=n; i++)
+    {
+        
+        Factorial *=i;
+    }
+
+    cout << n <<"! = " <<Factorial<< endl;
     return 0;
 }
