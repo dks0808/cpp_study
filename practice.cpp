364589fa5836e63fa10a2e871f52cc34245d96e6
light <agy91521947@gmail.com>
2024-02-19 17:22:22 +0900

    solving

--- practice.cpp
+++ practice.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+
+
 
 int main()
 {
-    int a, b, c;
-    cin >> a >> b;
-    int cap[a];
-    for(int i = 0; i <a ; i ++)
-    {
-        cin >> c;
-        cap[i] = c;
-    }
-    for(int i = 0; i < a; i++)
-    {
-        if(cap[i]<b)
-        {
-            cout << cap[i] << " ";
-        }
-    }
+    int c;
+    for(int i )
+
+    return 0;
+    
 }
 
