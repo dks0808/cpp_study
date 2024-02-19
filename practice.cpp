6c6f2f1ee38af32e56fa0b5de8a978ace094f5ad
light <agy91521947@gmail.com>
2024-02-19 23:19:03 +0900

    basketball

--- practice.cpp
+++ practice.cpp
@@ -1,55 +1,25 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-
-
-
 
 int main()
 {
-    int c;
-    
-    bool ft = true;
-    int arr[9];
-    int size = (sizeof(arr)/4);
-    int Max =0;
-    int lo = 0;
-    for(int i = 0; i < size; i++)
-    {        
-        cin >> c;
-        arr[i] = c;   
-    }
-   
-    
-    // for(int i = 0; i < size; i++)
-    // {
-    //     for(int x = 0; x < size; x++)
-    //         {
-    //             if(arr[i] < arr[x])
-    //             {
-    //                 Max_value = arr[x];
-    //                 lo = x+1;
-
-    //             }
-    //             else if(arr[i]> arr[x])
-    //             {
-    //                 Max_value = arr[i];
-    //                 lo = i+1;
-    //             }
-            
-    //         }
-        
-    // }
-    for(int i = 0; i <size ; i++ )
+    int N, M;
+    bool tf = true;
+    int i,j,k;
+    cin >> N >> M;
+    int arr[N];
+    int count = 1;
+    for(int i = 0; i < N; i++)
+        arr[i]=0;
+    while(count <= M)
     {
-        if(arr[i] > Max)
-        {
-            Max = arr[i];
-            lo = i;
-        }
+        cin >> i >> j >> k;
+        for(int x = i-1; x <=j-1; x++)
+            arr[x] = k;
+        count++;
     }
-
-    cout << Max << endl << lo+1;
+    for(int i =0; i<N; i++)
+        cout << arr[i] <<" ";
     return 0;
     
 }
