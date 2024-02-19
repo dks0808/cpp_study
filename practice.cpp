b7f690590d13cbd54ece9eb06ec98a649ea85acc
light <agy91521947@gmail.com>
2024-02-19 11:46:35 +0900

    searching lower number

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a, b;
+    cin >> a >>b
+    arrary
+    for(int i = 0; i <=a ; i++)
+    {
+        cin >>
+    }
+    return 0;
+}
