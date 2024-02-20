b7faaf59a098f953bea28f0f803a266a6bac14fe
light <agy91521947@gmail.com>
2024-02-20 23:11:03 +0900

    assignment check

--- practice.cpp
+++ practice.cpp
@@ -3,51 +3,23 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-    int N,M;
-    cin >> N >> M;
-    int arr[N] ;
-    int i, j;
-    bool tf = true;
-    int count =1;
-    for(int i = 0; i < N ; i ++ )
-    {
-        arr[i] = i + 1;
-    }
-    while(count <= M)
-    {
-        cin >> i >> j;
-        int temp;
-        temp = arr[i-1];
-        arr[i-1] = arr[j-1];
-        arr[j-1] = temp;
-        count++;
+    int p ;
+    int n = 30;
+    int arr[n] = {};
+    
 
+    for(int i = 0; i <28; i++)
+    {
+        cin >> p;
+        arr[p-1] = p;
+    }
+    for(int i = 1; i<=n; i++)
+    {
+        if(arr[i-1] != i)
+            cout << i <<endl;
+    }
+   
 
-    }
-    for(int i = 0; i < N; i ++)
-    {
-        cout << arr[i] << " ";
-    }        
-=======
-    int N, M;
-    bool tf = true;
-    int i,j,k;
-    cin >> N >> M;
-    int arr[N];
-    int count = 1;
-    for(int i = 0; i < N; i++)
-        arr[i]=0;
-    while(count <= M)
-    {
-        cin >> i >> j >> k;
-        for(int x = i-1; x <=j-1; x++)
-            arr[x] = k;
-        count++;
-    }
-    for(int i =0; i<N; i++)
-        cout << arr[i] <<" ";
->>>>>>> 6c6f2f1ee38af32e56fa0b5de8a978ace094f5ad
     return 0;
     
 }
