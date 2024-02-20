d4f3d22ee707a14b1d8544ddb5aaca6bf5520ebd
light <agy91521947@gmail.com>
2024-02-20 22:31:58 +0900

    changing ball in basket

--- practice.cpp
+++ practice.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-
-
-
 
 int main()
 {
-    int c;
-    for(int i )
+    int N,M;
+    cin >> N >> M;
+    int arr[N] ;
+    int i, j;
+    bool tf = true;
+    int count =1;
+    for(int i = 0; i < N ; i ++ )
+    {
+        arr[i] = i + 1;
+    }
+    while(count <= M)
+    {
+        cin >> i >> j;
+        int temp;
+        temp = arr[i-1];
+        arr[i-1] = arr[j-1];
+        arr[j-1] = temp;
+        count++;
 
+
+    }
+    for(int i = 0; i < N; i ++)
+    {
+        cout << arr[i] << " ";
+    }        
     return 0;
     
 }
