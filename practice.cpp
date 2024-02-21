1a352db0f79b215a84c997c4fdde8cc4729bffa5
light <agy91521947@gmail.com>
2024-02-21 15:19:04 +0900

    complete array

--- practice.cpp
+++ practice.cpp
@@ -1,41 +1,27 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    int a = 10;
-    int arr[a] ;
-    int count = 0;
-    for(int i = 0; i < a; i++)
+    int N;
+    cin >> N ;
+    int arr[N] ;
+    int count ;
+    for(int i = 0; i < N; i++)
     {
-        int p;
-        cin >> p;
-        int a = p%42;
-        arr[i] = a;
-    }
-
-    for(int i = 0; i < a; i++)
-    {
-        for(int x =0; x <a; x++)
-        {
-            if(arr[i]<arr[x])
-            {
-                int temp = arr[i];
-                arr[i] = arr[x];
-                arr[x] = temp;
-            }
-        }
-    }
-    for(int i = 0; i <a; i++)
-    {
-        if(arr[i] != arr[i+1])
-            count ++;
-    }
-    cout <<  count;
-
-    for(int i = 0; i <a; i++)
-        cout << arr[i];
+        cin >> arr[i];
         
+    }
+    int V;
+    cin >> V;
+
+    for(int x=0; x<N; x++)
+    {
+        if(arr[x] == V)
+            count ++;  
+    }  
+    cout <<count;
     return 0;
     
 }
