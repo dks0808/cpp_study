2f669ac4565ad888f1bec1ccbb68b9a880f37b18
light <agy91521947@gmail.com>
2024-02-24 18:16:29 +0900

    number 11718

--- practice.cpp
+++ practice.cpp
@@ -1,45 +1,12 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
-
 int main()
 {
-    string n1, n2;
-    char temp1, temp2;
-    cin >> n1 >> n2;
-    temp2 = n2[0];
-    n2[0] = n2[2];
-    n2[2] = temp2;
+    char a[101];
 
-    temp1 = n1[0];
-    n1[0] = n1[2];
-    n1[2] = temp1;
+    while(fgets(a,sizeof(a), stdin) != NULL)
+        printf("%s" ,a);
     
-    if(n1[0]>n2[0])
-    {
-        
-        cout << n1;
-    }
-    else if(n1[0]<n2[0])
-    {
-        
-        cout << n2;
-    }
-    else{
-        if(n1[1] > n2[1])
-            cout << n1;
-        else if(n1[1] < n2[1])
-            cout << n2;
-        
-        else
-        {
-            if(n1[2] > n2[2] )
-                cout << n1;
-            else if(n1[2]<n2[2])
-                cout << n2;
-            else if(n1[2]==n2[2])
-                cout<<n1;
-        }
-    }
     return 0;
 }
