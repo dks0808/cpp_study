e3c2c64935e327d08e9d54d5dee7efc5c0887488
light <agy91521947@gmail.com>
2024-02-27 15:56:38 +0900

    silver5

--- practice.cpp
+++ practice.cpp
@@ -3,10 +3,36 @@
 using namespace std;
 int main()
 {
-    char a[101];
+    int N;
+    int xmin, ymin ; 
+    int xmax, ymax = 0;
+    int x,y;
+    cin >> N;
+    int arrx[N];
+    int arry[N];
 
-    while(fgets(a,sizeof(a), stdin) != NULL)
-        printf("%s" ,a);
+    for(int i = 0; i < N ; i++)    
+        cin >> arrx[i] >> arry[i];
+
+    for(int i = 0; i < N ; i++)
+    {    
+        if(arrx[i]>xmax)
+            xmax = arrx[i];
+        if(arry[i] > ymax)
+            ymax =arry[i];
+    }
+    int colx = xmax;
+    int rowy = ymax;
+    int arrxy[rowy][colx];
+
+    for(int i = rowy-1 ; i>=0; i--)
+    {
+        for(int j = 0; j < colx; j++)
+            arrxy[i][j] = 
+    }
+
+
+    // array reset and size declear
     
     return 0;
 }
