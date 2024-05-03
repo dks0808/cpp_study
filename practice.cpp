3ff733cf343d876830771a02eb9864043f161681
light <agy91521947@gmail.com>
2024-05-03 19:03:39 +0900

    6-2algorithm

--- practice.cpp
+++ practice.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-int main(){
-    int N;
-    cin >> N;
-    int arr1[N][2];
-    for(int i =0; i< N; i++)
-        cin >> arr1[i][0] >> arr1[i][1];
-    for(int i =0; i<N; i++){
-        for(int j =i+1; j<N; j++){
-            if(arr1[i][0] > arr1[j][0]){
-                int temp1 = arr1[i][0];
-                arr1[i][0] = arr1[j][0];
-                arr1[j][0] = temp1;
-                int temp2 = arr1[i][1];
-                arr1[i][1] = arr1[j][1];
-                arr1[j][1] = temp2;
-            }
-            else if(arr1[i][0]==arr1[j][0] && arr1[i][1] > arr1[j][1]){
-                    int temp1 = arr1[i][0];
-                    arr1[i][0] = arr1[j][0];
-                    arr1[j][0] = temp1;
-                    int temp2 = arr1[i][1];
-                    arr1[i][1] = arr1[j][1];
-                    arr1[j][1] = temp2;
-            }
-        }
+
+void generate_passwords(vector<char>& letters, int N, int idx, string password) {
+    // 기저 사례: 비밀번호의 길이가 N에 도달하면 출력
+    if (password.length() == N) {
+        cout << password << endl;
+        return;
     }
-    
-    for(int i =0; i <N; i++){
-        for(int j =0; j<2; j++)
-            cout << arr1[i][j] <<" ";
-        cout << endl;
+
+    // 현재 인덱스부터 letters의 끝까지 순회하면서 비밀번호 생성
+    for (int i = idx; i < letters.size(); ++i) {
+        // 현재 문자를 비밀번호에 추가
+        string new_password = password + letters[i];
+        // 다음 문자를 포함하여 재귀 호출
+        generate_passwords(letters, N, i + 1, new_password);
     }
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<char> letters(M);
+    for (int i = 0; i < M; ++i) {
+        cin >> letters[i];
+    }
+
+    // 알파벳 순으로 정렬
+    sort(letters.begin(), letters.end());
+
+    // 비밀번호 생성
+    generate_passwords(letters, N, 0, "");
+
     return 0;
 }
